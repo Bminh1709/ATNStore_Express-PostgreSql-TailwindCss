e4cb9d1c3d54d679aa1622d2a89b8e37dfda8157
github_username <you@example.com>
2023-10-19 22:40:30 +0700

    production

--- databasebg.js
+++ databasebg.js
@@ -1,12 +1,21 @@
 const {Client} = require('pg')
 
+// const client = new Client({
+//     host: "localhost",
+//     user: "postgres",
+//     port: 5432,
+//     password: "minh",
+//     database: "atnstore"
+// });
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "minh",
-    database: "atnstore"
+    connectionString: 'postgres://atn_user:6TQrd8ITsFjFabZ7DwcZmA1K8lGrvJ7x@dpg-ckoin4s1tcps7394563g-a.oregon-postgres.render.com/atn?ssl=true',
 });
 
-client.connect();
+client.connect()
+    .then(() => {
+        console.log('Connected to the database successfully!');
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database:', error);
+    });
 module.exports = client;
