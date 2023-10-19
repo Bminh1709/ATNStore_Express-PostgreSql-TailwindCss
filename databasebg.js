eb3f520d487719c5f3c1703c9c57eda80f91b5d4
Bui Quang Minh <119041798+Bminh1709@users.noreply.github.com>
2023-10-19 22:27:05 +0700

    Update databasebg.js

--- databasebg.js
+++ databasebg.js
@@ -1,11 +1,7 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "minh",
-    database: "atnstore"
+    connectionString: 'postgres://atn_user:6TQrd8ITsFjFabZ7DwcZmA1K8lGrvJ7x@dpg-ckoin4s1tcps7394563g-a.oregon-postgres.render.com/atn?ssl=true',
 });
 
 client.connect();
