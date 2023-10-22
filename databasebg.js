6ecd1fada3b909563de0cf0d6c79ad9f57c5cc12
Bui Quang Minh <119041798+Bminh1709@users.noreply.github.com>
2023-10-22 22:49:32 +0700

    Update databasebg.js

--- databasebg.js
+++ databasebg.js
@@ -9,6 +9,7 @@ const {Client} = require('pg')
 // });
 const client = new Client({
     connectionString: 'postgres://atn_user:6TQrd8ITsFjFabZ7DwcZmA1K8lGrvJ7x@dpg-ckoin4s1tcps7394563g-a/atn?ssl=true',
+    ssl: { rejectUnauthorized: false }
 });
 
 client.connect()
